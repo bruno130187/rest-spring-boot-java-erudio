0c5b0177808b18280ce5f55a46672d934e9c8288
bruno130187 <bruno130187@gmail.com>
2022-12-16 16:40:22 -0300

    first commit

---
+++ src/main/java/br/com/erudio/restspringbootjavaerudio/RestSpringBootJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restspringbootjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootJavaErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erudio/restspringbootjavaerudio/RestSpringBootJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restspringbootjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
