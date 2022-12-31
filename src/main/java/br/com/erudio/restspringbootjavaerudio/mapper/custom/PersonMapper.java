d72bc51bc3fe43c6a81ec608d446a833187bbb7c
bruno130187 <bruno130187@gmail.com>
2022-12-31 17:02:24 -0300

    sessao 14 finalizada

--- src/main/java/br/com/erudio/restspringbootjavaerudio/mapper/custom/PersonMapper.java
+++ src/main/java/br/com/erudio/restspringbootjavaerudio/mapper/custom/PersonMapper.java
@@ -11,7 +11,7 @@ public class PersonMapper {
 
     public PersonVOV2 convertEntityToVo(Person person) {
         PersonVOV2 vo = new PersonVOV2();
-        vo.setId(person.getId());
+        vo.setKey(person.getId());
         vo.setFirstName(person.getFirstName());
         vo.setLastName(person.getLastName());
         vo.setAddress(person.getAddress());
@@ -22,7 +22,7 @@ public class PersonMapper {
 
     public Person convertVoToEntity(PersonVOV2 person) {
         Person entity = new Person();
-        entity.setId(person.getId());
+        entity.setId(person.getKey());
         entity.setFirstName(person.getFirstName());
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
